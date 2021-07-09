ddb78b1b9ca3cb9f41174b06c64acfe92e518e4b
Roberto Sousa <robertosousa186@gmail.com>
2021-07-08 23:44:57 -0300

    test

--- example1.js
+++ example1.js
@@ -1,3 +1,5 @@
+```
 rsconf = { _id: "mdbDefGuide", members: [ {_id: 0, host: "localhost:27017"}, {_id: 1, host: "localhost:27018"}, {_id: 2, host: "localhost:27019"} ] }
 rs.initiate(rsconf)
 rs.status()
+```
