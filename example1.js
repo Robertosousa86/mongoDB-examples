c7902cc1cb71b93869810b20bf698cb0ba30c598
Roberto Sousa <robertosousa186@gmail.com>
2021-07-14 01:18:38 -0300

    deleted

--- example1.js
+++
@@ -1,5 +0,0 @@
-```
-rsconf = { _id: "mdbDefGuide", members: [ {_id: 0, host: "localhost:27017"}, {_id: 1, host: "localhost:27018"}, {_id: 2, host: "localhost:27019"} ] }
-rs.initiate(rsconf)
-rs.status()
-```
